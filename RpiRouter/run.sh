f9b2dc392168bb72056c643c06b75b44ec90bc2f
micmoc95 <mic.morat@gmail.com>
2026-01-10 11:16:26 +0100

    Update run.sh

--- RpiRouter/run.sh
+++ RpiRouter/run.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-echo "Enabling IP forwarding..."
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-echo "Setting up iptables rules..."
-iptables -t nat -A POSTROUTING -o enu1 -j MASQUERADE
-iptables -A FORWARD -i enu1 -o end0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i end0 -o enu1 -j ACCEPT
-
-echo "Router setup complete."
-tail -f /dev/null  # Keep the container running
+lsusb -v
+tail -f /dev/null
