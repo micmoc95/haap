808f2c76f3f47ac3b68d6e8f022dfb26e8645f25
micmoc95 <mic.morat@gmail.com>
2026-01-11 12:28:55 +0100

    Update run.py

--- HaDialer/run.py
+++ HaDialer/run.py
@@ -1,10 +1,13 @@
-import os, requests
+import os, requests, logging
 from flask import Flask, request
 
 SUPERVISOR = "http://supervisor"
 TOKEN = os.environ["SUPERVISOR_TOKEN"]
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("hadialer")
+log.info("Starting ...")
 
 def ha(url, data=None):
     return requests.post(
@@ -23,7 +26,7 @@ ha("/core/api/services/hadialer/dial", {
 @app.route("/dial", methods=["POST"])
 def dial():
     data = request.json or {}
-    print("Commande reçue:", data)
+    log.info("Commande reçue:", data)
     return "OK"
 
 app.run(host="0.0.0.0", port=8124, debug=True)
