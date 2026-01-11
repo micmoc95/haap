4102639edce941fd240b160649f48215500dac98
micmoc95 <mic.morat@gmail.com>
2026-01-11 20:45:07 +0100

    Delete HaDialer/hadialer directory

--- HaDialer/hadialer/__init__.py
+++
@@ -1,9 +0,0 @@
-import aiohttp                          
-from homeassistant.core import ServiceCall
-                                          
-async def async_setup(hass, config):      
-    async def call_addon(call: ServiceCall):
-        async with aiohttp.ClientSession() as s:
-                await s.post("http://addon:8124/dial", json={"num": call.data["num"]} )
-    hass.services.async_register("hadialer", "dial", call_addon)
-    return True    
